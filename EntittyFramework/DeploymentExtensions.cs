466697d35554879d7e01480f887ea67be7f3ab4d
Chris Dibbs <chris@dibbs.tech>
2022-02-08 11:49:27 -0500

    retry for database updates

---
+++ EntittyFramework/DeploymentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DibbsTech.Common.EntittyFramework {
+    public static class DeploymentExtensions {
+        public static async Task TryDeployment(this DbContext database) {
+            Func<Task> deploy = () => {
+                database.Database.Migrate();
+                return Task.CompletedTask;
+            };
+
+            await deploy.Retry();
+        }
+    }
+}
---
+++ TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DibbsTech.Common {
+    public static class TaskExtensions {
+        public static async Task Retry(this Func<Task> action) {
+            await Retry(action, TimeSpan.FromSeconds(5));
+        }
+        public static async Task Retry(this Func<Task> action, TimeSpan backoff, int max = 10) {
+            var exceptions = new List<Exception>();
+
+            for(var ii = 0; ii<max; ii++) {
+                try {
+                    await action();
+                    return;
+                } catch (Exception e){
+                    exceptions.Add(e);
+                    await Task.Delay(backoff * Math.Pow(2, ii));
+                }
+            }
+            throw new AggregateException(exceptions);
+        }
+    }
+}
